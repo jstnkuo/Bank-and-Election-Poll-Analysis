5d04f7437283e6b0cf1878e19b1d952d7b66046c
Justin Kuo <112037668+jstnkuo@users.noreply.github.com>
2022-12-27 22:53:15 -0800

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-budget_data_csv = os.path.join("Instructions", "PyBank", "Resources", "budget_data.csv")
+budget_data_csv = os.path.join("Resources", "budget_data.csv")
 
 with open(budget_data_csv) as csvfile:
     csv_reader = csv.reader(csvfile)
